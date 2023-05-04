7e0fc6ef295e736fed31dcc23879b39cb1ca4e39
Ana Beatriz Marques <anafm@student.dei.uc.pt>
2023-05-04 11:52:00 +0100

    it works

--- urls.py
+++ urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("try/", views.trying , name="trying"),
     path("index/", views.index, name="index"),
     path("login/",views.login, name="login"),
 ]
--- views.py
+++ views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
 
 def login(request):
-    return HttpResponse("Hello, world. You're at the login.")
+    return HttpResponse("Hello, world. You're at the login endpoint.")
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    return HttpResponse("Hello, world. You're at the index endpoint.")
+
+
+def trying(request):
+    return HttpResponse("Hello, world. You're at the trying endpoint.")
 
 
